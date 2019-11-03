5f18c0141dd6c7e0fee17d8943fe5e21bc271692
Brian Carcich <BrianTCarcich@gmail.com>
2019-11-03 07:38:08 -0500

    2018/05 PartII

--- 2018/05/05.py
+++ 2018/05/05.py
@@ -1,16 +1,31 @@
 import sys
 
 if "__main__" == __name__:
+
+  ### build ['Aa','aA','Bb',...,'zZ']
+
   pairs = sum([[s.upper()+s,s+s.upper()]
                for s in 'abcdefghijklmnopqrstuvwxyz'
               ],[])
 
   s = sys.stdin.read(50010).strip()
 
-  L = len(s) + 10
+  ### remove [replace] string from s
+  rrsfs = lambda input_string,remme:input_string.replace(remme,'')
 
-  while len(s) < L:
-    L = len(s)
-    for pair in pairs: s = s.replace(pair,'')
 
-  print(len(s))
+  def partI(s):
+    """Find shortest length after recursively removing all pairs"""
+    L = len(s) + 10
+
+    while len(s) < L:
+      L = len(s)
+      for pair in pairs: s = s.replace(pair,'')
+    return L
+
+  print(partI(s))
+
+  def partII(s):
+    return min(map(partI,[rrsfs(rrsfs(s,d[0]),d[1]) for d in pairs]))
+
+  print partII(s)
