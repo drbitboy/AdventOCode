8c2254fcea9d6b8aacb4f3f72092bfc9ef10ead5
Brian Carcich <BrianTCarcich@gmail.com>
2019-11-05 06:40:13 -0500

    2018/day/24 code; almost done, starting testin

--- 2018/24/24.py
+++ 2018/24/24.py
@@ -4,32 +4,55 @@ import sys
 do_debug = "DEBUG" in os.environ
 
 class GROUP(object):
+  """Class to hold properties and execute behavior of group of either
+immunity system units or infection units
 
-  ### Atack types
+"""
+
+  ##################################
+  ### Class-wide properties
+  ##################################
+
+  ### - Atack types
   st_attacks = 'cold slashing bludgeoning radiation fire'.split()
-  ###dt_attacks = dict([(s,i) for i,s in enumerate(st_attacks)])
 
+  ##################################
   def __repr__(self):
+    if not do_debug: return self.group_name
     return '{0}/{1}/{2}:{3}/{4}/i:{5}/w:{6}'.format(
       self.units
       ,self.hp
       ,self.attack_damage
       ,self.attack_type
       ,self.initiative
-      ,','.join([k for k in GROUP.st_attacks if not self.dt_iw[k]])
-      ,','.join([k for k in GROUP.st_attacks if 2 == self.dt_iw[k]])
+      ,','.join([k for k in GROUP.st_attacks if not self.dt_iw_multiples[k]])
+      ,','.join([k for k in GROUP.st_attacks if 2 == self.dt_iw_multiples[k]])
       )
 
-  def __init__(self,toks):
-    """Constructor for a group of units, immune system or infection"""
+  ##################################
+  def __init__(self,toks, group_name='Unknown group'):
+    """Constructor for a group of immune system or infection units
+
+Argument [toks] is an iterator of strings from a line of the input file
+e.g. this input line:
+
+  18 units each with 729 hit points (weak to fire; immune to cold,
+  slashing) with an attack that does 8 radiation damage at initiative 10
+
+yields this list of tokens (toks argument):
+
+  ['18', 'units', ..., 'initiative', 10]
+"""
 
     ### Placeholder for which opposing group is targeting this group
     ### N.B. this will be set during the target selection phase, and
     ###      cleared at the end of the attacking phase
     self.clear_attacker()
 
-    ### Save original string, assume single-spaced
+    ### Save original line, assume toks are single-spaced
+    ### Save group name
     self.original = ' '.join(toks)
+    self.group_name = group_name
 
     ### Parse from back end
     ###   ... with an attack that does 8 cold damage at initiative 10
@@ -61,25 +84,30 @@ class GROUP(object):
     assert 'points' == toks.pop()
 
     ### Initialize all damage multipliers to 1
-    self.dt_iw = dict([(s,1,) for s in GROUP.st_attacks])
+    self.dt_iw_multiples = dict([(s,1,) for s in GROUP.st_attacks])
 
     ### Parse middle section immunities and/or weaknesses
     ###   .. (weak to fire; immune to cold, slashing) ...
+    ###
+    ###   N.B. immunity and weakness keywords tokens are always trailed
+    ###        by a single character that is not part of the keyword e.g
+    ###        "fire;" or "cold," or "slashing)"
 
-    ### Set immunity/weakness multiple to None to ensure parsed data are
-    ### correct
-    iw = None
+    ### Set immunity/weakness multiple to None to ensure parsed data
+    ### syntax and grammar is correct
+    iw_multiple = None
 
-    ### Loop over tokens
+    ### Loop over reversed tokens i.e. from end of list
     while toks:
+
       tok = toks.pop()
 
       if tok.endswith('weak'):
 
         ### - Weaknesses double the damage of the given attack types
 
-        assert iw is None
-        iw = 2
+        assert iw_multiple is None
+        iw_multiple = 2
         assert 'to' == toks.pop()
         continue
 
@@ -87,8 +115,8 @@ class GROUP(object):
 
         ### - Immunities avoid all damage of the given attack types
 
-        assert iw is None
-        iw = 0
+        assert iw_multiple is None
+        iw_multiple = 0
         assert 'to' == toks.pop()
         continue
 
@@ -96,96 +124,152 @@ class GROUP(object):
       ###   or [weak to] tokens parsed
 
       assert tok[:-1] in GROUP.st_attacks
-      assert not (iw is None)
-      self.dt_iw[tok[:-1]] = iw
+      assert not (iw_multiple is None)
+      self.dt_iw_multiples[tok[:-1]] = iw_multiple
 
       ### - Clear iw multiple if a semi-colon is encountered
-      if tok.endswith(';'): iw = None
+      if tok.endswith(';'): iw_multiple = None
 
       ### End of [while toks:] loop
 
 
+  ##################################
   def effective_power(self):
     """Calculate effective power"""
+
     return self.units * self.attack_damage
 
-  def select_target(self,target):
-    """Select target by 1) which target has not already been selected
-as a target by another group; 2) to which target the most damage will be
-done; 3) target with largest effective power; 3) target with highest
-initiative
 
-Return a tuple with a value representing each metric
+  ##################################
+  def select_target_key(self,target):
+    """max(<list>,key=<key>) key function to select target for self
+
+Metrics:
+1) which target has not already been selected as a target by another
+   group;
+2) to which target the most damage will be done by self;
+3) target with largest effective power;
+3) target with highest initiative
+
+Return a four-tuple with a value representing each of those metrics
+
+Usage:
+
+  selected_target = max(list_targets, attacker.select_target)
+  selected_target.set_attacker(attacker)
 
 """
-
-    return (target.targeted_by is None ? 1 : 0
+    return (target.targeted_by is None) and 1 or 0
            ,target.calculate_damage(self)
            ,target.effective_power()
            ,target.initiative
            )
 
+
+  ##################################
   def calculate_damage(self, targeted_by):
-    """Calculate attack damage based on attack type"""
+    """Calculate attack damage to self by attack from targeted_by"""
+
+    ### If targeted_by is not a group (i.e. is None), then return zero
 
     if not isinstance(targeted_by,GROUP): return 0
 
-    return ( targeted_by.units
-           * targeted_by.attack_damage
-           * dt_iw[targeted_by.attack_type]
+    ### Scale effective power of targeted_by by any immunity or weakness
+
+    return ( targeted_by.effective_power()
+           * self.dt_iw_multiples[targeted_by.attack_type]
            )
 
+
+  ##################################
   def run_attack(self):
     """Run an attack from GROUP self.targeted_by"""
+
     damage = self.calculate_damage(self.targeted_by)
 
     if damage > 0:
-      units_lost = damage / self.hp
-      if units_lost >= self.units:
-        self.units = 0
-      else
-        self.units -= units_lost
+      units_lost = min([damage / self.hp, self.units])
+      self.units -= units_lost
 
     self.clear_attacker()
 
+
+  ##################################
   def clear_attacker(self):
     """Clear attacker"""
 
     self.set_attacker(None)
 
-  def set_attacker(self,attacker):
-    """Set attacker for next attacking phase"""
 
-    self.targeted_by = attacker
+  ##################################
+  def set_attacker(self,attacker):
+    """Set attacker for next attacking phase
+
+- Do not allow overwriting one attacker with another
+- Allow neither attacker nor defender to have no units
+
+"""
+    if (self.targeted_by is None) or (attacker is None):
+
+      if attacker is None or (self.units > 1 and attacker.units > 1):
+
+        self.targeted_by = attacker
+
 
 ### End of class GROUP
 ########################################################################
-### Ordering keys
 
-def doselection_key(group):
-  """Selection order is by decreasing effective power, then
-decreasing initiative (higher initiative chooses first)
+########################################################################
+### Ordering key functions
+###
+### N.B. Lists will be created with sorted(<list>,key=<key>), and then
+###      processed from the **end** of the list via .pop(), so the
+###      ordering of the list is that the first GROUP to be processed,
+###      that is the group with the highest-sorted metrics, will be last
+###      on the sorted list, and the last GROUP to be processed will be
+###      first on the list
+########################################################################
 
-Return a tuple with a value representing each metric
+
+####################################
+def selection_order_key(group):
+  """Selection order key for sorted list with 1st selector last in list
+
+Metrics:
+1) by decreasing effective power,
+2) decreasing initiative (higher initiative chooses first)
+
+Return a two-tuple with a value representing each metric
 
 """
 
   return (group.effective_power(),group.initiative,)
 
-def doattack_key(group):
-  """Attack order is by decreasing initiative (higher initiative attacks
-first)
+
+####################################
+def attack_order_key(group):
+  """Attack order key for sorted list with 1st attacker last in list
+
+Metric is that higher initiative attacks first.
+
+N.B. Each attacker will be in the .targeted_by member of the group it is
+     attacking, so it is the ***targeted*** entities that will be sorted
 
 """
 
-  return group.initiative
+  ### GROUPs that are not targeted will have None as their .targeted_by
+  ### member; sort them last:
+  if not isinstance(group.targeted_by,GROUP): return -1
+
+  return group.targeted_by.initiative
 
 
 ########################################################################
 class OHDEER(object):
-  """Class to hold a deer's immunities and infections"""
+  """Class to hold the states of a deer's immunities and infections"""
 
   def __init__(self,iter):
+    """Parse iterable of strings that contain descriptions of groups"""
 
     self.lt_immune = list()
     self.lt_infection = list()
@@ -199,18 +283,66 @@ class OHDEER(object):
       L = len(toks)
 
       if 2 == L:
-        assert 'System:' == toks.pop()
+        side = toks.pop()
+        assert 'System:' == side
         assert 'Immune' == toks.pop()
         team = self.lt_immune
 
       elif 1 == L:
-        assert 'Infection:' == toks.pop()
+        side = toks.pop()
+        assert 'Infection:' == side
         team = self.lt_infection
 
       elif 17 < L:
-        team.append(GROUP(toks))
+        group_name = '{0} group {1}'.format(side[:-1], len(team) + 1)
+        team.append(GROUP(toks, group_name=group_name))
 
       assert not len(toks)
+
+    ### End of .__init__ constructor
+    ####################################################################
+
+  ##################################
+  def run_fight(self):
+    """Run one round of target_selection and attacking"""
+
+    lt_two = [self.lt_immune, self.lt_infection]
+
+    ### Selection phase
+
+    lt_target = lt_two[-1]
+
+    for lt in lt_two:
+
+      ### Sort attacker list per selection order key
+      lt_selection_ordered = sorted(lt,key=selection_order_key)
+
+      while lt_selection_ordered:
+        lt_group = lt_selection_ordered.pop()
+
+        ### Find group to attack in target list
+        target_group = max(lt_target,key=lt_group.select_target_key)
+
+        ### And target that list
+        target_group.set_targeted_by(lt_group)
+
+      ### Switch target list
+      lt_target = lt_two[0]
+
+    ### Attacking phase:  buld attack-sorted list; run attacks
+
+    lt_attack = sorted(sum(lt_two,[]),key=attack_order_key)
+
+    while lt_attack: lt_attack.pop().run_attack()
+
+
+  ##################################
+  def sums_units(self,lt):
+    """Sum numbers of units in the arrays of groups"""
+
+    return ( sum([group.units for group in self.lt_immune])
+           , sum([group.units for group in self.lt_infection])
+           )
 
 
 if "__main__" == __name__ and sys.argv[1:]:
