ad626b4865445887bba788009b18d252486a3fed
Brian Carcich <BrianTCarcich@gmail.com>
2019-12-17 13:19:35 -0500

    2019/08/ part 2

--- 2019/08/08.py
+++ 2019/08/08.py
@@ -6,11 +6,21 @@ layer_count = lambda str_layer,c: len([None
                                        if cl==c
                                       ])
 
+########################################################################
 def part1(str_layer):
   n0 = layer_count(str_layer,'0')
   n1 = layer_count(str_layer,'1')
   n2 = layer_count(str_layer,'2')
   return n0,n1*n2
+########################################################################
+
+########################################################################
+def part2(str_layer):
+  pixels = list(map(lambda lt:''.join(lt).replace('2','')[0],zip(*str_layer)))
+  img_data = ''.join(pixels).replace('1','@').replace('0',' ')
+  return '\n'.join([img_data[i:i+w] for i in range(0,area,w)])
+########################################################################
+
 
 w,h = 25,6
 area = w * h
@@ -31,4 +41,4 @@ if "__main__" == __name__:
               ]
 
   print(sorted(map(part1,lt_layers))[0])
-  #print(part2())
+  print(part2(lt_layers))
